c78c4cd7b5f50c703f2e7d841a75d170d10df7a1
Death <mathrox431@yahoo.com>
2019-07-12 11:28:05 -0400

    feat:Added styles.

--- src/main.js
+++ src/main.js
@@ -22,7 +22,7 @@ class Main extends Component {
             </li>
           </ul>
           <div className="content">
-            <Route path="/" component={Home} />
+            <Route exact path="/" component={Home} />
             <Route path="/stuff" component={Stuff} />
             <Route path="/contact" component={Contact} />
           </div>
